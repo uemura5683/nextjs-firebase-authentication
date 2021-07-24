00940edc3c2ac16d417210f5475a57ee1c29f0a2
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-07-24 14:26:07 +0900

    npmインストール調整など

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,5 @@
 import 'firebase/auth'
 import 'firebase/firestore'
-
 import firebase from 'firebase/app'
 
 const config = {
@@ -10,7 +9,7 @@ const config = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APPID,
+  appId: process.env.FIREBASE_APPID
 }
 // initializeを複数回走らせない
 if (firebase.apps.length === 0) {
